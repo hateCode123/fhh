834932c048fd868eac08d84b614a183166d6eac1
caoyuan1 <caoyuan1@ifeng.com>
2018-12-12 10:52:48 +0800

    fix: 删除models不存在的dictionary

--- client/pc/common/store.js
+++ client/pc/common/store.js
@@ -2,7 +2,6 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import user from '../common/models/user';
-import dictionary from '../common/models/dictionary';
 import inputInfoSimple from '../../pc/register/inputInfoSimple/models';
 import inputInfo from '../../pc/register/inputInfo/models';
 // import dictionary from '../common/models/dictionary';
@@ -13,7 +12,6 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export default createStore(
     combineReducers({
         user,
-        dictionary,
         inputInfoSimple,
         inputInfo,
         // dictionary,
